68c8694d3637785aa1325bcb17dd7987b7571ed6
Sotheavuth Nguon <sotheavuth.nguon@yahoo.com>
2024-11-27 22:45:24 +0700

    Updated

---
+++ sotheavuthlibrary/src/main/java/com/sotheavuthnguon/sotheavuthlibrary/HelloWorldPrinter.java
@@ -0,0 +1,11 @@
+package com.sotheavuthnguon.sotheavuthlibrary;
+
+import android.util.Log;
+
+public class HelloWorldPrinter {
+    private static final String TAG = "HelloWorldPrinter";
+
+    public static void printHelloWorld() {
+        Log.d(TAG, "🟢🟢🟢🟢🟢🟢 Hello, World! 🟢🟢🟢🟢🟢🟢");
+    }
+}
